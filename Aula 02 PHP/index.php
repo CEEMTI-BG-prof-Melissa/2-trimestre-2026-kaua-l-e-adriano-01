5005774829ebfbc21319c7f0455f8bb6ce2efed8
KLzin <kaua.lvbenfica@aluno.edu.es.gov.br>
2026-07-01 07:44:51 -0300

    Create index.php

---
+++ Aula 02 PHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula PHP</title>
+</head>  
+<body>
+    <?php
+        $nome1="Kauã";
+        $idade1=16;
+
+        $nome2="Adriano";
+        $idade2=16;
+
+        echo "Meu nome é " .$nome1. " eu tenho " .$idade1. " anos de idade, a minha dupla se chama " .$nome2. " e ele tambem tem " .$idade2. " de idade."
+    ?>
+
+
+</body>
+</html>
